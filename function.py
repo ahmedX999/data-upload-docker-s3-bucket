670df8cdfe33462d203d0a98ca31126254819fbd
ahmedX999 <aaittaleb.354@gmail.com>
2024-06-07 01:09:07 +0100

    lambda modified

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from The S3 Bucket Lambda function! 2024'
+        'body': 'Hello from The S3 Bucket Lambda function! 2024 '
     }
