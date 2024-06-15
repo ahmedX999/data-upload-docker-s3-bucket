de7bbd304a2c81590e6826ba889988e16cb92c35
ahmedX999 <104841511+ahmedX999@users.noreply.github.com>
2024-06-15 01:48:59 +0100

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,10 @@
 def lambda_handler(event, context):
+    encrypted_access_key = os.environ['ENCRYPTED_ACCESS_KEY']
+    encrypted_secret_key = os.environ['ENCRYPTED_SECRET_KEY']
+    print("encrypted_access_key")
+    print(encrypted_access_key)
+    print("encrypted_secret_key")
+    print(encrypted_secret_key)
     return {
         'statusCode': 200,
         'body': 'Hello from The S3 Bucket Lambda function! 2024 '
