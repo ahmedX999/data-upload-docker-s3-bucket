b6b99f57859fc5d87d163d9316fa6f086fd65324
Maryame44 <162489563+Maryame44@users.noreply.github.com>
2024-06-27 14:39:32 +0100

    Update function.py

--- function.py
+++ function.py
@@ -1,8 +1,5 @@
-import os
 import json
 import boto3
-from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
 from botocore.exceptions import ClientError
 import base64
 
@@ -16,29 +13,21 @@ def decrypt(ciphertext):
         return None
 
 def get_secret(secret_name):
+    # Crée un client Secrets Manager
     client = boto3.client('secretsmanager')
+
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
         print(f"Erreur lors de la récupération du secret: {e}")
         return None
 
+    # Décrypte le secret à l'aide de la clé KMS associée.
     secret = get_secret_value_response['SecretString']
     return json.loads(secret)
 
-def create_drive_service(client_id, client_secret, refresh_token):
-    creds = Credentials(
-        None,
-        refresh_token=refresh_token,
-        token_uri='https://oauth2.googleapis.com/token',
-        client_id=client_id,
-        client_secret=client_secret
-    )
-    drive_service = build('drive', 'v3', credentials=creds)
-    return drive_service
-
 def lambda_handler(event, context):
-    secret_name = "my-google-drive-secret"  #  le nom de  secret
+    secret_name = "my-secret"  #  le nom de secret
 
     secrets = get_secret(secret_name)
     if not secrets:
@@ -47,41 +36,43 @@ def lambda_handler(event, context):
             'body': json.dumps('Erreur lors de la récupération des secrets.')
         }
 
-    encrypted_client_id = secrets.get('ENCRYPTED_CLIENT_ID')
-    encrypted_client_secret = secrets.get('ENCRYPTED_CLIENT_SECRET')
-    encrypted_refresh_token = secrets.get('ENCRYPTED_REFRESH_TOKEN')
+    encrypted_access_key = secrets.get('ENCRYPTED_ACCESS_KEY')
+    encrypted_secret_key = secrets.get('ENCRYPTED_SECRET_KEY')
+    encrypted_region = secrets.get('ENCRYPTED_REGION')
 
-    if not encrypted_client_id or not encrypted_client_secret or not encrypted_refresh_token:
+    if not encrypted_access_key or not encrypted_secret_key or not encrypted_region:
         return {
             'statusCode': 400,
             'body': json.dumps('Les informations d\'identification chiffrées sont requises.')
         }
 
-    client_id = decrypt(encrypted_client_id)
-    client_secret = decrypt(encrypted_client_secret)
-    refresh_token = decrypt(encrypted_refresh_token)
+    access_key = decrypt(encrypted_access_key)
+    secret_key = decrypt(encrypted_secret_key)
+    region = decrypt(encrypted_region)
     
-    if not client_id or not client_secret or not refresh_token:
+    if not access_key or not secret_key or not region:
         return {
             'statusCode': 500,
             'body': json.dumps('Erreur lors du déchiffrement des informations d\'identification.')
         }
 
     try:
-        drive_service = create_drive_service(client_id, client_secret, refresh_token)
-        
-        # Récupérer la liste des fichiers du Google Drive
-        results = drive_service.files().list(pageSize=10).execute()
-        items = results.get('files', [])
-        
-        response = {
+        session = boto3.Session(
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region
+        )
+        s3 = session.client('s3')
+
+        response = s3.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        return {
             'statusCode': 200,
-            'body': json.dumps(items)
+            'body': json.dumps(buckets)
         }
+
     except Exception as e:
-        response = {
+        return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            'body': json.dumps(f"Erreur lors de la connexion à S3: {str(e)}")
         }
-    
-    return response
